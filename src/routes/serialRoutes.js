5e96b017f251ed966e2645841892d54475608cfc
unknown <vexixongaming02@gmail.com>
2025-02-19 10:34:06 +0700

    g

--- src/routes/serialRoutes.js
+++ src/routes/serialRoutes.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const { authenticate } = require('../middleware/auth');
-const SerialController = require('../controllers/serialController');
+const express = require("express");
+const { authenticate } = require("../middleware/auth");
+const SerialController = require("../controllers/serialController");
 
 const router = express.Router();
 
-router.get('/read', authenticate, SerialController.readData);
-router.post('/send', authenticate, SerialController.sendData);
+router.get("/read", SerialController.readData);
+router.post("/send", SerialController.sendData);
+// router.get('/read', authenticate, SerialController.readData);
+// router.post('/send', authenticate, SerialController.sendData);
 
 module.exports = router;
